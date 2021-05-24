b6d83ff3cea5657db43d8b4d7b924b646020e29c
gs-maker <gerald.ssali@gmail.com>
2021-05-24 06:07:50 +0300

    gsap animation added

--- Day 18/main.js
+++ Day 18/main.js
@@ -8,3 +8,20 @@ const navSlide = () => {
 };
 
 navSlide();
+
+gsap.from(".workSelect", {
+	// scrollTrigger: ".workSelect",
+	y: 500,
+	duration: 1,
+	stagger: 0.1,
+	ease: "back-in",
+	scrub: 3,
+	// scrub: 3,
+});
+
+gsap.from(".intro", {
+	x: -1000,
+	duration: 0.5,
+	stagger: 0.2,
+	ease: "back-in",
+});
